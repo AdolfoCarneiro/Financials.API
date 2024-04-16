c1dd6048d29a656f5e6a8abacd378501346f6bf3
Adolfo Carneiro <adolfo.carneiro@gowsolucoes.com>
2024-04-16 09:43:22 -0300

    fix: pegando status code depois da execucao da action no logging middleware

--- Financials.API/Middlewares/LoggingMiddleware.cs
+++ Financials.API/Middlewares/LoggingMiddleware.cs
@@ -28,14 +28,16 @@ namespace Financials.API.Middlewares
             }
             finally
             {
-                var statusCode = context.Response.StatusCode;
-                var logLevel = statusCode >= 400 ? LogEventLevel.Error : LogEventLevel.Information;
+                
 
                 var originalBodyStream = context.Response.Body;
                 using var responseBody = new MemoryStream();
                 context.Response.Body = responseBody;
 
                 await _next(context);
+
+                var statusCode = context.Response.StatusCode;
+                var logLevel = statusCode >= 400 ? LogEventLevel.Error : LogEventLevel.Information;
 
                 context.Response.Body.Seek(0, SeekOrigin.Begin);
                 var text = await new StreamReader(context.Response.Body).ReadToEndAsync();
