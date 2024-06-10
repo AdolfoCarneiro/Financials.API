b0a069d734cbc606020cda78aa3a2ca755eb32e8
Adolfo Carneiro <adolfocarneiro555@gmail.com>
2024-06-10 20:46:43 -0300

    feat: add controller

---
+++ Financials.API/Controllers/v1.0/TransacaoController.cs
@@ -0,0 +1,29 @@
+﻿using Financials.Core.DTO;
+using Financials.Services.RequestsResponses.Base;
+using Financials.Services.RequestsResponses.Transacao;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Financials.API.Controllers.v1._0
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+
+    public class TransacaoController(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ApplicationResponse<TransacaoDTO>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ApplicationResponse<TransacaoDTO>), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(ApplicationResponse<TransacaoDTO>), (int)HttpStatusCode.InternalServerError)]
+        public async Task<ActionResult<ApplicationResponse<TransacaoDTO>>> CriarTransacao([FromBody] RegristrarTransacaoRequest request)
+        {
+            var response = await _mediator.Send(request);
+            return this.GetResponse(response);
+        }
+    }
+}
