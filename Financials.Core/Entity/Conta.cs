7ea65fdf9532ef4f0f4624de017481725d2edaf7
Adolfo Carneiro <adolfo.carneiro@gowsolucoes.com>
2025-05-27 15:44:11 -0300

    remove import nao usado

--- Financials.Core/Entity/Conta.cs
+++ Financials.Core/Entity/Conta.cs
@@ -2,7 +2,6 @@
 using Financials.Core.Interfaces;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Financials.Core.Entity
 {
